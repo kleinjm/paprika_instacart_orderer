949b8b63fa929d87998266825845e83744d4c4ef
James Klein <kleinjm007@gmail.com>
2019-10-15 18:50:41 -0400

    users and custom edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(
+        :email, :password, :current_password, :paprika_email,
+        :paprika_password, :instacart_email, :instacart_password,
+        :instacart_default_store
+      )
+    end
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  attr_encrypted :instacart_password, :paprika_password
+end
